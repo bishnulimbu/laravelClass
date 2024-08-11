985e977ea2d48d93421549b805817318e47e45a4
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-08-11 10:02:45 +0545

    list.blade file

---
+++ laravel1/todo1/resources/views/product/list.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Page</title>
+</head>
+<body>
+    <h1>Product Page</h1>
+    <p>Welcome to the product page!</p>
+    <a href="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg">
+        <img src="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/master/pass/phonepicutres-TA.jpg" alt="Product Image" style="width: 300px; height: auto;">
+    </a>
+</body>
+</html>
