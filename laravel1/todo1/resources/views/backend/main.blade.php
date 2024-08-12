bb682396840f7be0f34ffbb6caf23edf9caddddb
Bishnu Limbu <subashlimbu987654321@gmail.com>
2024-08-12 16:30:11 +0545

    same as before

--- laravel1/todo1/resources/views/backend/main.blade.php
+++ laravel1/todo1/resources/views/backend/main.blade.php
@@ -4,18 +4,18 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title></title>
-        <link href="css/style.css" rel="stylesheet">
+
+        @include('backend.includes.css')
     </head>
     <body>
     @include('backend.includes.header')
     @include('backend.includes.nav')
-
-{{-- body --}}
-@yield('content')
-{{-- /body --}}
+    @yield('content')
 
 
-    @include('backend.includes.footer')
+
+
+    @include('backend.includes.js')
 
     </body>
 </html>
